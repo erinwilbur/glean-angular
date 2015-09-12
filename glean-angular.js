9d2c7002340ea869a2351d9e8db6208388267460
Erin Wilbur <erinmariewilbur@gmail.com>
2015-09-12 10:05:28 -0500

    add iron router and intial pages and navigation

--- glean-angular.js
+++ glean-angular.js
@@ -1,4 +1,4 @@
-if (Meteor.isClient) {
+/* if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
@@ -20,4 +20,14 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
-}
+} */
+Router.route('/', function() {
+  // render the Home template with a custom data context
+  this.render('Home', {data: {title: 'Glean'}});
+});
+
+// when you nav to "/one" auto render the template name "One"
+Router.route('/one');
+
+// when you nav to "/two" auto render the template name "Two"
+Router.route('/two');
